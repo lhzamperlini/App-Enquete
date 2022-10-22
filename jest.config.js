b231bb4d7e39989700d3fe03103efc89854f1bbd
lhzamperlini <lhzamperlini@gmail.com>
2022-10-22 18:38:40 -0300

    fix: jest now work

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/src/'],
+  collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
@@ -7,12 +8,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
   }
 }
--- src/controller.spec.ts
+++ src/controller.spec.ts
@@ -1,5 +1,3 @@
-import { describe } from 'yargs'
-
 describe('', () => {
   test('', () => {
     expect(1).toBe(1)
