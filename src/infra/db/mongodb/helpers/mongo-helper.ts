d1c0e1c80cbd5a5a57d44d9986c6fc2e3da0064d
lhzamperlini <lhzamperlini@gmail.com>
2022-11-06 15:12:20 -0300

    chore: add correct Mongodb-jest version

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
-  async connect (uri: string) {
+  async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
